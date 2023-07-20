5c06c6ccfb1188d472dce3ac0cffd673ad5f70a0
Raul Guerrero <raul.guerrero@devfactory.com>
2023-07-20 01:01:10 -0600

    Initial Commit

---
+++ ChromePatcher/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChromePatcher
+//
+//  Created by Raul Guerrero on 20/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
